1bb969824453dd6b4400a84145846a7381b6afe3
kskrao219 <keshava.balaji@anywhere.co>
2023-09-03 00:31:24 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -13,10 +13,13 @@ const client = new Octokit(
                 });
 const { context } = github;
 const pull_number = context.payload.pull_request.number;
-// const owner = context.repo.owner;
+const owner = context.payload.repository.owner.login ;
+const repo = context.payload.pull_request.base.repo.name;
 // const repo = context.repo.repo;
 console.log("context , pr :::" , context);
 console.log("pr no " , pull_number);
+console.log("owner " , owner);
+console.log("repo " , repo);
 // const JiraApiUrl = `${orgUrl}/rest/api/2/issue/${jiraId}` ;
 try {
 //  console.log(owner,repo,pull_number);
