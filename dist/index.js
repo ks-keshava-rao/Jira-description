450d2f2de9cafc63ee634f626bee8b191787c0f7
kskrao219 <keshava.balaji@anywhere.co>
2023-09-03 00:38:05 +0530

    testing

--- dist/index.js
+++ dist/index.js
@@ -13409,32 +13409,43 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(5127);
 const github = __nccwpck_require__(3134);
 const { Octokit } = __nccwpck_require__(1563);
+async function addprdescription() {
+    const token = core.getInput('token');
+    const jiraId = core.getInput('jiraId');
+    const orgUrl = core.getInput('orgUrl');
+    const jiraToken = core.getInput('jiraToken');
+    const sonarQubeUrl = core.getInput('sonarQubeUrl')
+    const jiraUsername = core.getInput('JiraUsername')
+    const client = new Octokit(
+        {
+            auth: token
+        });
+    const { context } = github;
+    const pull_number = context.payload.pull_request.number;
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.pull_request.base.repo.name;
+    const body = "This is a markdown\nthis is new line \n *Heading*\nthis is under heading"
+    // const repo = context.repo.repo;
+    console.log("context , pr :::", context);
+    console.log("pr no ", pull_number);
+    console.log("owner ", owner);
+    console.log("repo ", repo);
+    // const JiraApiUrl = `${orgUrl}/rest/api/2/issue/${jiraId}` ;
+    try {
+        //  console.log(owner,repo,pull_number);
+        await client.rest.pulls.update({
+            owner,
+            repo,
+            pull_number,
+            body,
+        })
+    }
+    catch (e) {
+        core.setFailed(`process failed with ::: ${e.message}`);
+    }
+}
+addprdescription();
 
-const token = core.getInput('token');
-const jiraId = core.getInput('jiraId');
-const orgUrl = core.getInput('orgUrl');
-const jiraToken = core.getInput('jiraToken');
-const sonarQubeUrl = core.getInput('sonarQubeUrl')
-const jiraUsername = core.getInput('JiraUsername')
-const client = new Octokit(
-                    {auth: token
-                });
-const { context } = github;
-const pull_number = context.payload.pull_request.number;
-const owner = context.payload.repository.owner.login ;
-const repo = context.payload.pull_request.base.repo.name;
-// const repo = context.repo.repo;
-console.log("context , pr :::" , context);
-console.log("pr no " , pull_number);
-console.log("owner " , owner);
-console.log("repo " , repo);
-// const JiraApiUrl = `${orgUrl}/rest/api/2/issue/${jiraId}` ;
-try {
-//  console.log(owner,repo,pull_number);
-}
-catch(e){
-    core.setFailed(`process failed with ::: ${e.message}`);
-}
 })();
 
 module.exports = __webpack_exports__;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
-
-const token = core.getInput('token');
-const jiraId = core.getInput('jiraId');
-const orgUrl = core.getInput('orgUrl');
-const jiraToken = core.getInput('jiraToken');
-const sonarQubeUrl = core.getInput('sonarQubeUrl')
-const jiraUsername = core.getInput('JiraUsername')
-const client = new Octokit(
-                    {auth: token
-                });
-const { context } = github;
-const pull_number = context.payload.pull_request.number;
-const owner = context.payload.repository.owner.login ;
-const repo = context.payload.pull_request.base.repo.name;
-// const repo = context.repo.repo;
-console.log("context , pr :::" , context);
-console.log("pr no " , pull_number);
-console.log("owner " , owner);
-console.log("repo " , repo);
-// const JiraApiUrl = `${orgUrl}/rest/api/2/issue/${jiraId}` ;
-try {
-//  console.log(owner,repo,pull_number);
+async function addprdescription() {
+    const token = core.getInput('token');
+    const jiraId = core.getInput('jiraId');
+    const orgUrl = core.getInput('orgUrl');
+    const jiraToken = core.getInput('jiraToken');
+    const sonarQubeUrl = core.getInput('sonarQubeUrl')
+    const jiraUsername = core.getInput('JiraUsername')
+    const client = new Octokit(
+        {
+            auth: token
+        });
+    const { context } = github;
+    const pull_number = context.payload.pull_request.number;
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.pull_request.base.repo.name;
+    const body = "This is a markdown\nthis is new line \n *Heading*\nthis is under heading"
+    // const repo = context.repo.repo;
+    console.log("context , pr :::", context);
+    console.log("pr no ", pull_number);
+    console.log("owner ", owner);
+    console.log("repo ", repo);
+    // const JiraApiUrl = `${orgUrl}/rest/api/2/issue/${jiraId}` ;
+    try {
+        //  console.log(owner,repo,pull_number);
+        await client.rest.pulls.update({
+            owner,
+            repo,
+            pull_number,
+            body,
+        })
+    }
+    catch (e) {
+        core.setFailed(`process failed with ::: ${e.message}`);
+    }
 }
-catch(e){
-    core.setFailed(`process failed with ::: ${e.message}`);
-}
+addprdescription();
