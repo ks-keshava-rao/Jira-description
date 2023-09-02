3c37e1a742d7faba870e63c46424ffedaffc5ac3
kskrao219 <keshava.balaji@anywhere.co>
2023-09-03 00:43:02 +0530

    testing

--- dist/index.js
+++ dist/index.js
@@ -13424,7 +13424,7 @@ async function addprdescription() {
     const pull_number = context.payload.pull_request.number;
     const owner = context.payload.repository.owner.login;
     const repo = context.payload.pull_request.base.repo.name;
-    const body = "This is a markdown\nthis is new line \n *Heading*\nthis is under heading"
+    const body = "This is a test description in a paragraph\n\n*Why*\nThis denotes what is the issue\n\n*What*\nThis means how the problem is solved and what are the changes that have been done to solve the issue and what was the approach \n\n* test description with new changes and new description\n* test description with second changes .\n\n[https://mail.google.com/mail/u/0/?ogbl#inbox|https://mail.google.com/mail/u/0/?ogbl#inbox]"
     // const repo = context.repo.repo;
     console.log("context , pr :::", context);
     console.log("pr no ", pull_number);
