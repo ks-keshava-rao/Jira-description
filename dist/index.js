f13bf19ee205fb2bd590c923d06627fb9ebcb604
kskrao219 <keshava.balaji@anywhere.co>
2023-09-06 11:59:14 +0530

    test

--- dist/index.js
+++ dist/index.js
@@ -13317,7 +13317,7 @@ module.exports = async({authToken,jiraApiUrl}) => {
 module.exports = {
     constructBodyTemplate: ({fields,sonarQubeUrl,JiraUrl})=>{
         const { description , summary} = fields;
-        const body = `# Description\n### \n${summary}\n\n ${description}\n\n## Jira Ticket\n${JiraUrl}\n\n${ sonarQubeUrl ? `\n\n## Sonar Results:\n${sonarQubeUrl}`:""}\n\n## Checklist:\n - [ ] Code follows the coding style guidelines.\n - [ ] Tests have been added or updated.\n - [ ] Documentation has been updated if necessary.`
+        const body = `# Description\n\n### ${summary}\n\n ${description}\n\n## Jira Ticket\n${JiraUrl}\n\n${ sonarQubeUrl ? `\n\n## Sonar Results:\n${sonarQubeUrl}`:""}\n\n## Checklist:\n - [ ] Code follows the coding style guidelines.\n - [ ] Tests have been added or updated.\n - [ ] Documentation has been updated if necessary.`
         return body;
     }
 }
